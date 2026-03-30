bc56f9866ad7fe607a73ef186bf9616e7b1ae53e
chillcicada <2210227279@qq.com>
2026-03-30 14:47:21 +0800

    chore: update aasvg

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,7 @@
+use aasvg::RenderOptions;
 use wasm_minimal_protocol::*;
 
 initiate_protocol!();
-
-#[derive(serde::Deserialize)]
-struct RenderOptions {
-    backdrop: bool,
-    disable_text: bool,
-    spaces: u32,
-    stretch: bool,
-}
-
-impl From<RenderOptions> for aasvg::RenderOptions {
-    fn from(opts: RenderOptions) -> Self {
-        aasvg::RenderOptions {
-            backdrop: opts.backdrop,
-            disable_text: opts.disable_text,
-            spaces: opts.spaces,
-            stretch: opts.stretch,
-        }
-    }
-}
 
 #[wasm_func]
 pub fn render_with_options(input: &[u8], options: &[u8]) -> Result<Vec<u8>, String> {
